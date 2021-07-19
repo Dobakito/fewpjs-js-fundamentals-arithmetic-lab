1be315f4b16b4349b61a965f683f3299ba7b0535
Dobakito <mdobak44@icloud.com>
2021-07-19 14:18:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random() > 0)
+
+let num3 = 14
+let num4 = 5
+var mod = num3 % num4
+
+var max = Math.max(20)
